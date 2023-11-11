2bff3542b847224e745167ef1237a36a9633dbc3
YYYJJMM <itsjju4@gmail.com>
2023-11-11 22:46:15 +0900

    feat: first commit - 깃테스트1 - 깃테스트2

---
+++ src/com/bjpublic/intellij/tutorial/IntentionActions.java
@@ -0,0 +1,9 @@
+package com.bjpublic.intellij.tutorial;
+
+import java.util.Scanner;
+
+public class IntentionActions {
+    String html = "<html><head><title>Intention Actions </title></head></html>";
+
+
+}
---
+++ src/com/bjpublic/intellij/tutorial/testJju1.java
@@ -0,0 +1,7 @@
+package com.bjpublic.intellij.tutorial;
+
+public class testJju1 {
+
+    System.out.println("테스트1");
+
+}
